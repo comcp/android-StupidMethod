5c34e9b5fd777f8b7512822d45e3e9154ef24230
(๑• . •๑) <comcp@126.com>
2015-06-08 18:02:42 +0800

    添加FragmentPara

--- StupidMethod/src/com/stupid/method/adapter/XViewPagerAdapter.java
+++ StupidMethod/src/com/stupid/method/adapter/XViewPagerAdapter.java
@@ -1,5 +1,6 @@
 package com.stupid.method.adapter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import android.support.v4.app.Fragment;
@@ -10,21 +11,38 @@ import com.stupid.method.app.XFragment;
 
 public class XViewPagerAdapter extends FragmentPagerAdapter {
 
-	List<Class<? extends XFragment>> mFragments;
-	XActivity xActivity;
+	List<FragmentParam> mFragments;
 
 	public XViewPagerAdapter(XActivity activity,
 			List<Class<? extends XFragment>> fragments) {
 		super(activity.getSupportFragmentManager());
+		mFragments = new ArrayList<XViewPagerAdapter.FragmentParam>();
+		FragmentParam param = null;
+		for (Class<? extends XFragment> class1 : fragments) {
+			param = new FragmentParam();
+			param.xFragment = class1;
+			mFragments.add(param);
+		}
+
+	}
+
+	public XViewPagerAdapter(XActivity activity, List<FragmentParam> fragments,
+			int i) {
+		super(activity.getSupportFragmentManager());
 		mFragments = fragments;
-		xActivity = activity;
 	}
 
 	@Override
 	public Fragment getItem(int position) {
 		try {
 
-			return mFragments.get(position).newInstance();
+			if (mFragments.get(position).fragment == null) {
+				mFragments.get(position).fragment = (XFragment) mFragments
+						.get(position).xFragment.newInstance();
+			}
+			mFragments.get(position).fragment
+					.setData(mFragments.get(position).data);
+			return mFragments.get(position).fragment;
 
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -38,4 +56,10 @@ public class XViewPagerAdapter extends FragmentPagerAdapter {
 		return mFragments.size();
 	}
 
+	public static class FragmentParam {
+		private XFragment fragment;
+		public Class<? extends XFragment> xFragment;
+		public Object data;
+
+	}
 }
