dc6c9bcd474184bb36d2461c952039b4036c7668
X <comcp@126.com>
2016-07-08 21:48:23 +0800

    Update AppManager.java

--- StupidMethod/src/com/stupid/method/app/AppManager.java
+++ StupidMethod/src/com/stupid/method/app/AppManager.java
@@ -15,7 +15,6 @@ import org.apache.http.conn.util.InetAddressUtils;
 
 import android.app.Application;
 import android.content.Context;
-import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
@@ -27,7 +26,6 @@ import android.os.Handler;
 import android.util.DisplayMetrics;
 import android.view.WindowManager;
 
-import com.alibaba.fastjson.JSON;
 import com.stupid.method.util.FileUtils;
 import com.stupid.method.util.SharedPreferencesHelper;
 import com.stupid.method.util.XLog;
@@ -46,49 +44,28 @@ public class AppManager extends Application {
 	// public static String DIR_LOGS = "%s%s/logs" + File.separator;
 
 	private static AppManager instance = null;
-	private Point screenSize;
-	private int versionCode = -1;
 
+	public synchronized static AppManager getInstance() {
+
+		return instance;
+	}
+
+	private Point screenSize;
+
+	private int versionCode = -1;
 	private ExecutorService mExecutor = Executors.newFixedThreadPool(3);
+
 	private Handler mHandler = new Handler();
 
-	public final boolean post(Runnable r) {
-
-		return mHandler.post(r);
-	}
-
-	public String getNameSpace() {
-
-		return getPackageName();
-	}
-
-	@Override
-	public void onCreate() {
-		super.onCreate();
-		instance = this;
-	}
-
 	File mRoot;
+
+	public void execute(Runnable command) {
+		mExecutor.execute(command);
+	}
 
 	public File getAppCacheDir() {
 		initDir();
 		return mRoot;
-	}
-
-	public File getOrCreateDataBase(String dbname) throws IOException {
-
-		File file = new File(getAppCacheDir(), dbname);
-		if (file.getParentFile().exists()) {
-			file.getParentFile().mkdirs();
-		}
-		if (file.exists()) {
-			file.createNewFile();
-		}
-		return file;
-	}
-
-	public ExecutorService getExecutorService() {
-		return mExecutor;
 	}
 
 	public File getAppDataBaseDir() {
@@ -99,8 +76,8 @@ public class AppManager extends Application {
 		return file;
 	}
 
-	public File getAppImgDir() {
-		File file = new File(getAppCacheDir(), "IMGS");
+	public File getAppFileDir() {
+		File file = new File(getAppCacheDir(), "FILES");
 		if (!file.exists()) {
 			file.mkdirs();
 		}
@@ -108,8 +85,8 @@ public class AppManager extends Application {
 
 	}
 
-	public File getAppFileDir() {
-		File file = new File(getAppCacheDir(), "FILES");
+	public File getAppImgDir() {
+		File file = new File(getAppCacheDir(), "IMGS");
 		if (!file.exists()) {
 			file.mkdirs();
 		}
@@ -126,120 +103,8 @@ public class AppManager extends Application {
 
 	}
 
-	private synchronized void initDir() {
-
-		if (mRoot == null) {
-			File path = getPath();
-			File root = new File(FileUtils.join(path.getAbsolutePath(),
-					"Android", "Data", getNameSpace()));
-			if (!root.exists()) {
-				root.mkdirs();
-			}
-			try {
-				File testRW = new File(root, "test");
-				testRW.createNewFile();
-				testRW.delete();
-			} catch (IOException e) {
-				root = getCacheDir();
-			} finally {
-
-			}
-			mRoot = root;
-		}
-		// DIR_FILE = String.format(DIR_FILE, root, NAMESPACE);
-		// DIR_PICS = String.format(DIR_PICS, root, NAMESPACE);
-		// DIR_THUMB = String.format(DIR_THUMB, root, NAMESPACE);
-		// SIR_MEDIAS = String.format(SIR_MEDIAS, root, NAMESPACE);
-		// DIR_TEMP = String.format(DIR_TEMP, root, NAMESPACE);
-		// DIR_LOGS = String.format(DIR_LOGS, root, NAMESPACE);
-		// File file = new File(DIR_FILE);
-		// if (!file.exists())
-		// file.mkdirs();
-		// file = new File(DIR_LOGS);
-		// if (!file.exists())
-		// file.mkdirs();
-		//
-		// file = new File(DIR_PICS);
-		// if (!file.exists())
-		// file.mkdirs();
-		// file = new File(DIR_THUMB);
-		// if (!file.exists())
-		// file.mkdirs();
-		// file = new File(SIR_MEDIAS);
-		// if (!file.exists())
-		// file.mkdirs();
-		// file = new File(DIR_TEMP);
-		// if (!file.exists())
-		// file.mkdirs();
-
-	}
-
-	public synchronized static AppManager getInstance() {
-
-		return instance;
-	}
-
-	/**
-	 * 获取软件版本号
-	 * 
-	 * @param context
-	 * @return
-	 */
-	public int getVersionCode() {
-
-		if (versionCode == -1) {
-			PackageManager packageManager = this.getApplicationContext()
-					.getPackageManager();
-			/*
-			 * getInstalledApplications 返回当前设备上安装的应用包集合
-			 * ApplicationInfo对应着androidManifest
-			 * .xml中的application标签。通过它可以获取该application对应的信息
-			 */
-
-			PackageInfo pinfo;
-			try {
-				pinfo = packageManager.getPackageInfo(this
-						.getApplicationContext().getPackageName(),
-						PackageManager.GET_CONFIGURATIONS);
-				versionCode = pinfo.versionCode;
-
-			} catch (NameNotFoundException e) {
-
-				e.printStackTrace();
-			} finally {
-				pinfo = null;
-
-			}
-		}
-		return versionCode;
-	}
-
-	/**
-	 * @return the screenSize point( x:width y:height)
-	 */
-	public Point getScreenSize() {
-
-		if (screenSize == null) {
-			WindowManager wm = (WindowManager) this
-					.getSystemService(Context.WINDOW_SERVICE);
-
-			DisplayMetrics dm = new DisplayMetrics();
-			wm.getDefaultDisplay().getMetrics(dm);
-			screenSize = new Point(dm.widthPixels, dm.heightPixels);
-		}
-		return screenSize;
-	}
-
-	/**
-	 * get wifi mac address
-	 * 
-	 * @need "android.permission.ACCESS_WIFI_STATE" in you manifest.xml
-	 *       uses-permission
-	 * **/
-	public String getLocalMacAddress() {
-		WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-		WifiInfo info = wifi.getConnectionInfo();
-		return info.getMacAddress();
+	public ExecutorService getExecutorService() {
+		return mExecutor;
 	}
 
 	/**
@@ -291,82 +156,33 @@ public class AppManager extends Application {
 		return "";
 	}
 
-	public void execute(Runnable command) {
-		mExecutor.execute(command);
+	/**
+	 * get wifi mac address
+	 * 
+	 * @need "android.permission.ACCESS_WIFI_STATE" in you manifest.xml
+	 *       uses-permission
+	 * **/
+	public String getLocalMacAddress() {
+		WifiManager wifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+		WifiInfo info = wifi.getConnectionInfo();
+		return info.getMacAddress();
 	}
 
-	private static String getKey(Object key) {
+	public String getNameSpace() {
 
-		if (key == null) {
-			return "";
-		} else if (key instanceof String || key instanceof StringBuilder
-				|| key instanceof StringBuffer) {
-
-			return key.toString();
-		} else if (key instanceof Class) {
-			return ((Class<?>) key).getSimpleName();
-		} else
-			return key.getClass().getSimpleName();
+		return getPackageName();
 	}
 
-	public AppManager setXmlBoolean(Object key, boolean value) {
-		getSharedPreferences().putBoolean(getKey(key), value);
-		return this;
-	}
+	public File getOrCreateDataBase(String dbname) throws IOException {
 
-	public boolean getXmlBoolean(Object key, boolean defValue) {
-		return getSharedPreferences().getBoolean(getKey(key), defValue);
-	}
-
-	public int getXmlInt(Object key, int defValue) {
-		return getSharedPreferences().getInt(getKey(key), defValue);
-	}
-
-	public AppManager setXmlInt(Object key, int value) {
-
-		getSharedPreferences().putInt(getKey(key), value);
-		return this;
-
-	}
-
-	public AppManager setXmlJSON(String key, Object value) {
-		getSharedPreferences().putJSON(key, value);
-		return this;
-	}
-
-	public <T> T getXmlJSON(Class<T> clazz) {
-		return getXmlJSON(clazz, clazz);
-
-	}
-
-	public <T> T getXmlJSON(Object key, Class<T> clazz) {
-		try {
-			return JSON.parseObject(
-					getSharedPreferences().getString(getKey(key), ""), clazz);
-		} catch (Exception e) {
-			return null;
+		File file = new File(getAppCacheDir(), dbname);
+		if (file.getParentFile().exists()) {
+			file.getParentFile().mkdirs();
 		}
-
-	}
-
-	public AppManager setXmlString(String key, String value) {
-		getSharedPreferences().putString(key, value);
-		return this;
-	}
-
-	public String getXmlString(Object key) {
-
-		return this.getXmlString(key, "");
-	}
-
-	public String getXmlString(Object key, String defValue) {
-
-		return getSharedPreferences().getString(getKey(key), defValue);
-	}
-
-	public SharedPreferencesHelper getSharedPreferences() {
-
-		return SharedPreferencesHelper.getCache(this, "AppData");
+		if (file.exists()) {
+			file.createNewFile();
+		}
+		return file;
 	}
 
 	public File getPath() {
@@ -387,12 +203,181 @@ public class AppManager extends Application {
 			return getCacheDir();
 	}
 
+	/**
+	 * @return the screenSize point( x:width y:height)
+	 */
+	public Point getScreenSize() {
+
+		if (screenSize == null) {
+			WindowManager wm = (WindowManager) this
+					.getSystemService(Context.WINDOW_SERVICE);
+
+			DisplayMetrics dm = new DisplayMetrics();
+			wm.getDefaultDisplay().getMetrics(dm);
+			screenSize = new Point(dm.widthPixels, dm.heightPixels);
+		}
+		return screenSize;
+	}
+
+	public SharedPreferencesHelper getSharedPreferences() {
+
+		return SharedPreferencesHelper.getCache(this, "AppData");
+	}
+
+	/**
+	 * 获取软件版本号
+	 * 
+	 * @param context
+	 * @return
+	 */
+	public int getVersionCode() {
+
+		if (versionCode == -1) {
+			PackageManager packageManager = this.getApplicationContext()
+					.getPackageManager();
+			/*
+			 * getInstalledApplications 返回当前设备上安装的应用包集合
+			 * ApplicationInfo对应着androidManifest
+			 * .xml中的application标签。通过它可以获取该application对应的信息
+			 */
+
+			PackageInfo pinfo;
+			try {
+				pinfo = packageManager.getPackageInfo(this
+						.getApplicationContext().getPackageName(),
+						PackageManager.GET_CONFIGURATIONS);
+				versionCode = pinfo.versionCode;
+
+			} catch (NameNotFoundException e) {
+
+				e.printStackTrace();
+			} finally {
+				pinfo = null;
+
+			}
+		}
+		return versionCode;
+	}
+
+	public boolean getXmlBoolean(String key, boolean defValue) {
+		return getSharedPreferences().getBoolean(key, defValue);
+	}
+
+	public int getXmlInt(String key, int defValue) {
+		return getSharedPreferences().getInt(key, defValue);
+	}
+
+	public <T> T getXmlJSON(Class<T> clazz) {
+		return getSharedPreferences().getJSON(clazz);
+
+	}
+
+	public <T> T getXmlJSON(String key, Class<T> clazz) {
+		try {
+			return getSharedPreferences().getJSON(key, clazz);
+		} catch (Exception e) {
+			return null;
+		}
+
+	}
+
+	public String getXmlString(String key) {
+
+		return getSharedPreferences().getString(key, "");
+	}
+
+	public String getXmlString(String key, String defValue) {
+
+		return getSharedPreferences().getString(key, defValue);
+	}
+
+	private synchronized void initDir() {
+
+		if (mRoot == null) {
+			File path = getPath();
+			File root = new File(FileUtils.join(path.getAbsolutePath(),
+					"Android", "Data", getNameSpace()));
+			if (!root.exists()) {
+				root.mkdirs();
+			}
+			try {
+				File testRW = new File(root, "test");
+				testRW.createNewFile();
+				testRW.delete();
+			} catch (IOException e) {
+				root = getCacheDir();
+			} finally {
+
+			}
+			mRoot = root;
+		}
+		// DIR_FILE = String.format(DIR_FILE, root, NAMESPACE);
+		// DIR_PICS = String.format(DIR_PICS, root, NAMESPACE);
+		// DIR_THUMB = String.format(DIR_THUMB, root, NAMESPACE);
+		// SIR_MEDIAS = String.format(SIR_MEDIAS, root, NAMESPACE);
+		// DIR_TEMP = String.format(DIR_TEMP, root, NAMESPACE);
+		// DIR_LOGS = String.format(DIR_LOGS, root, NAMESPACE);
+		// File file = new File(DIR_FILE);
+		// if (!file.exists())
+		// file.mkdirs();
+		// file = new File(DIR_LOGS);
+		// if (!file.exists())
+		// file.mkdirs();
+		//
+		// file = new File(DIR_PICS);
+		// if (!file.exists())
+		// file.mkdirs();
+		// file = new File(DIR_THUMB);
+		// if (!file.exists())
+		// file.mkdirs();
+		// file = new File(SIR_MEDIAS);
+		// if (!file.exists())
+		// file.mkdirs();
+		// file = new File(DIR_TEMP);
+		// if (!file.exists())
+		// file.mkdirs();
+
+	}
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+		instance = this;
+	}
+
 	@Override
 	public void onLowMemory() {
 		// clear all memory cached images when system is in low memory
 		// note that you can configure the max image cache count, see
 		// CONFIGURATION
 		com.androidquery.callback.BitmapAjaxCallback.clearCache();
+	}
+
+	public final boolean post(Runnable r) {
+
+		return mHandler.post(r);
+	}
+
+	public AppManager setXmlBoolean(String key, boolean value) {
+		getSharedPreferences().putBoolean(key, value);
+		return this;
+	}
+
+	public AppManager setXmlInt(String key, int value) {
+
+		getSharedPreferences().putInt(key, value);
+		return this;
+
+	}
+
+	public AppManager setXmlJSON(String key, Object value) {
+		getSharedPreferences().putJSON(key, value);
+		return this;
+	}
+
+	public AppManager setXmlString(String key, String value) {
+		getSharedPreferences().putString(key, value);
+		return this;
 	}
 
 }
